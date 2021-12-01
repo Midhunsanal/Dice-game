d6790fc01a8acc9651f8cfd60d58b937b9b635b9
Midhunsanal <92135997+Midhunsanal@users.noreply.github.com>
2021-12-01 14:05:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 $("button").click(function(){
   var randomNumber1 = Math.floor(Math.random()*6)+1;
-  var randomDiceImage ="images/dice" + randomNumber1 + ".png";
+  var randomDiceImage ="dice" + randomNumber1 + ".png";
   var image1 = document.querySelectorAll("img")[0];
   image1.setAttribute("src", randomDiceImage);
 
   var randomnumber2 = Math.floor(Math.random()*6)+1;
-  var randomDiceImage2= "images/dice"+randomnumber2+".png";
+  var randomDiceImage2= "dice"+randomnumber2+".png";
   var image2 = document.querySelectorAll("img")[1];
   image2.setAttribute("src", randomDiceImage2);
 
